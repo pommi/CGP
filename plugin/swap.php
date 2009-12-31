c9f886d787c5d33e3773d193e1a68bdb3a7628c2
Pim van den Berg <pim@nethuis.nl>
2009-12-31 13:09:03 +0100

    add swap_io support

--- plugin/swap.php
+++ plugin/swap.php
@@ -3,7 +3,6 @@
 # Collectd Swap plugin
 
 require_once 'conf/common.inc.php';
-require_once 'type/GenericStacked.class.php';
 require_once 'inc/collectd.inc.php';
 
 ## LAYOUT
@@ -12,28 +11,43 @@ require_once 'inc/collectd.inc.php';
 # swap/swap-free.rrd
 # swap/swap-used.rrd
 
-if ($_GET['t'] == 'swap_io') {
-	die_img('Error: swap_io not supported yet');
-	exit;
+switch($_GET['t']) {
+	case 'swap':
+		require_once 'type/GenericStacked.class.php';
+		$obj = new Type_GenericStacked($CONFIG['datadir']);
+		$obj->order = array('free', 'cached', 'used');
+		$obj->ds_names = array(
+			'free' => 'Free	',
+			'cached' => 'Cached  ',
+			'used' => 'Used	',
+		);
+		$obj->colors = array(
+			'free' => '00e000',
+			'cached' => '0000ff',
+			'used' => 'ff0000',
+		);
+		$obj->rrd_title = 'Swap utilization';
+		$obj->rrd_vertical = 'Bytes';
+	break;
+	case 'swap_io':
+		require_once 'type/GenericIO.class.php';
+		$obj = new Type_GenericIO($CONFIG['datadir']);
+		$obj->order = array('out', 'in');
+		$obj->ds_names = array(
+			'out' => 'Out',
+			'in' => 'In ',
+		);
+		$obj->colors = array(
+			'out' => '0000ff',
+			'in' => '00b000',
+		);
+		$obj->rrd_title = 'Swapped I/O pages';
+		$obj->rrd_vertical = 'Pages';
+	break;
 }
 
-$obj = new Type_GenericStacked($CONFIG['datadir']);
-$obj->order = array('free', 'cached', 'used');
-$obj->ds_names = array(
-	'free' => 'Free    ',
-	'cached' => 'Cached  ',
-	'used' => 'Used    ',
-);
-$obj->colors = array(
-	'free' => '00e000',
-	'cached' => '0000ff',
-	'used' => 'ff0000',
-);
 $obj->width = $width;
 $obj->heigth = $heigth;
-
-$obj->rrd_title = 'Swap utilization';
-$obj->rrd_vertical = 'Bytes';
 $obj->rrd_format = '%5.1lf%s';
 
 collectd_flush($obj->identifiers);
