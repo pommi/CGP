055e034d16f2d358fa6712c190fc8d3742e9531d
Julien Rottenberg <jrottenberg@app2v-trend.lax1.gnmedia.net>
2010-09-14 16:14:35 -0700

    plugin/nfs: remove ds_names to keep it simple

--- plugin/nfs.php
+++ plugin/nfs.php
@@ -17,55 +17,11 @@ $obj->data_sources = array('value');
 switch($obj->args['pinstance']) {
     case 'v2client':
         $obj->order = array('create', 'fsstat', 'getattr', 'link', 'lookup', 'mkdir', 'null', 'readdir', 'readlink', 'read', 'remove', 'rename', 'rmdir', 'root', 'setattr', 'symlink', 'wrcache', 'write');
-        $obj->ds_names = array(
-            'create'   => 'Create   ',
-            'fsstat'   => 'FSStat   ',
-            'getattr'  => 'GetAttr  ',
-            'link'     => 'Link     ',
-            'lookup'   => 'Lookup   ',
-            'mkdir'    => 'MkDir    ',
-            'null'     => 'Null     ',
-            'readdir'  => 'ReadDir  ',
-            'readlink' => 'ReadLink ',
-            'read'     => 'Read     ',   
-            'remove'   => 'Remove   ',
-            'rename'   => 'Rename   ',
-            'rmdir'    => 'RmDir    ',
-            'root'     => 'Root     ',
-            'setattr'  => 'SetAttr  ',
-            'symlink'  => 'SymLink  ',
-            'wrcache'  => 'WrCache  ',
-            'write'    => 'Write    ',
-        );
         $obj->generate_colors();
     break;
 
     case 'v3client':
         $obj->order = array('access', 'commit', 'create', 'fsinfo', 'fsstat', 'getattr', 'link', 'lookup', 'mkdir', 'mknod', 'null', 'pathconf', 'read', 'readdir', 'readdirplus', 'readlink', 'remove', 'rename', 'rmdir', 'setattr', 'symlink', 'write');
-        $obj->ds_names = array(
-            'getattr'     => 'GetAttr     ',
-            'setattr'     => 'SetAttr     ',
-            'lookup'      => 'Lookup      ',
-            'access'      => 'Access      ',
-            'readlink'    => 'ReadLink    ',
-            'read'        => 'Read        ',
-            'write'       => 'Write       ',
-            'create'      => 'Create      ',
-            'mkdir'       => 'MkDir       ',
-            'symlink'     => 'Symlink     ',
-            'mknod'       => 'MkNode      ',
-            'remove'      => 'Remove      ',
-            'rmdir'       => 'RmDir       ',
-            'rename'      => 'Rename      ',
-            'link'        => 'Link        ',
-            'readdir'     => 'ReadDir     ',
-            'readdirplus' => 'ReadDirPlus ',
-            'fsstat'      => 'FsStat      ',
-            'fsinfo'      => 'FsInfo      ',
-            'pathconf'    => 'PathConf    ',
-            'commit'      => 'Commit      ',
-            'null'        => 'Null        ',
-        );
         $obj->generate_colors();
 
     break;
