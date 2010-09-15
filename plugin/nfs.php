589111dddb1329b08c03ecc54ab3e97ff742f9f4
Pim van den Berg <pim@nethuis.nl>
2010-09-15 19:00:35 +0200

    plugin/nfs: clean up some duplicate code

--- plugin/nfs.php
+++ plugin/nfs.php
@@ -17,23 +17,20 @@ $obj->data_sources = array('value');
 switch($obj->args['pinstance']) {
     case 'v2client':
         $obj->order = array('create', 'fsstat', 'getattr', 'link', 'lookup', 'mkdir', 'null', 'readdir', 'readlink', 'read', 'remove', 'rename', 'rmdir', 'root', 'setattr', 'symlink', 'wrcache', 'write');
-        $obj->generate_colors();
     break;
 
     case 'v3client':
         $obj->order = array('access', 'commit', 'create', 'fsinfo', 'fsstat', 'getattr', 'link', 'lookup', 'mkdir', 'mknod', 'null', 'pathconf', 'read', 'readdir', 'readdirplus', 'readlink', 'remove', 'rename', 'rmdir', 'setattr', 'symlink', 'write');
-        $obj->generate_colors();
-
     break;
-
-
 }
+
 $obj->width = $width;
 $obj->heigth = $heigth;
 
 $obj->rrd_title = sprintf('NFS-%s Procedures', $obj->args['pinstance']);
 $obj->rrd_vertical = 'Procedures';
 $obj->rrd_format = '%5.2lf';
+$obj->generate_colors();
 
 collectd_flush($obj->identifiers);
 $obj->rrd_graph();
