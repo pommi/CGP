ebcc489d12493a5ee4807126325e284bb10f9469
Pim van den Berg <pim@nethuis.nl>
2010-10-04 14:50:53 +0200

    plugin: add bind plugin
    Very basic support for bind.

---
+++ plugin/bind.php
@@ -0,0 +1,20 @@
+<?php
+
+# Collectd bind plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/GenericStacked.class.php';
+require_once 'inc/collectd.inc.php';
+
+
+$obj = new Type_GenericStacked($CONFIG);
+$obj->width = $width;
+$obj->heigth = $heigth;
+$obj->generate_colors();
+
+$obj->rrd_title = sprintf('%s/%s', $obj->args['pinstance'], $obj->args['type']);
+$obj->rrd_format = '%5.1lf%s';
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
+
