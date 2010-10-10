d24eea595474f559627ea1ef85a7f2065bc7e92d
Pim van den Berg <pim@nethuis.nl>
2010-10-10 12:29:59 +0200

    plugin: add cpufreq plugin

---
+++ plugin/cpufreq.php
@@ -0,0 +1,22 @@
+<?php
+
+# Collectd CPUfreq plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/Default.class.php';
+require_once 'inc/collectd.inc.php';
+
+## LAYOUT
+# cpufreq/cpufreq-X.rrd
+
+$obj = new Type_Default($CONFIG);
+$obj->data_sources = array('value');
+$obj->width = $width;
+$obj->heigth = $heigth;
+$obj->generate_colors();
+$obj->rrd_title = 'CPU frequency scaling';
+$obj->rrd_vertical = 'Mhz';
+$obj->rrd_format = '%5.1lf%s';
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
