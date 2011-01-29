42f95b39f4ec31b42f8e7e293c83d69938d84e7a
Pim van den Berg <pim@nethuis.nl>
2011-01-29 19:16:39 +0100

    plugin: add dns plugin

---
+++ plugin/dns.php
@@ -0,0 +1,58 @@
+<?php
+
+# Collectd DNS plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/GenericStacked.class.php';
+require_once 'inc/collectd.inc.php';
+
+## LAYOUT
+# dns/
+# dns/dns_octets.rrd
+# dns/dns_opcode-X.rrd
+# dns/dns_qtype-X.rrd
+
+$obj = new Type_GenericStacked($CONFIG);
+$obj->width = $width;
+$obj->heigth = $heigth;
+switch($obj->args['type']) {
+	case 'dns_octets':
+		$obj->data_sources = array(
+			'queries',
+			'responses',
+		);
+		$obj->ds_names = array(
+			'queries'   => 'Queries',
+			'responses' => 'Responses',
+		);
+		$obj->colors = array(
+			'queries'   => '0000ff',
+			'responses' => '00ff00',
+		);
+		$obj->rrd_title = 'DNS traffic';
+		$obj->rrd_vertical = 'Bit/s';
+		$obj->rrd_format = '%5.1lf%s';
+	break;
+	case 'dns_opcode':
+		$obj->data_sources = array('value');
+		$obj->ds_names = array(
+			'value' => 'Queries/s',
+		);
+		$obj->colors = array(
+			'value' => '0000ff',
+		);
+		$obj->rrd_title = 'DNS Opcode Query';
+		$obj->rrd_vertical = 'Queries/s';
+		$obj->rrd_format = '%5.1lf%s';
+	break;
+	case 'dns_qtype':
+		$obj->data_sources = array('value');
+		$obj->generate_colors();
+		$obj->rrd_title = 'DNS QType';
+		$obj->rrd_vertical = 'Queries/s';
+		$obj->rrd_format = '%5.1lf%s';
+	break;
+}
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
