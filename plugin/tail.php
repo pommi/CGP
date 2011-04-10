d86d1743d7fa3aabed3ed241e899b22db9430f15
Pim van den Berg <pim@nethuis.nl>
2011-04-10 19:49:27 +0200

    plugin: add tail plugin

---
+++ plugin/tail.php
@@ -0,0 +1,18 @@
+<?php
+
+# Collectd tail plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/GenericStacked.class.php';
+require_once 'inc/collectd.inc.php';
+
+
+$obj = new Type_GenericStacked($CONFIG);
+$obj->width = $width;
+$obj->heigth = $heigth;
+
+$obj->rrd_title = sprintf('tail: %s (%s)', $obj->args['pinstance'], $obj->args['type']);
+$obj->rrd_format = '%5.1lf%s';
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
