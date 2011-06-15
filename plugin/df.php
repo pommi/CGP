9dd8be51f0469d21cd879604bcfff9cb05f40806
Pim van den Berg <pim@nethuis.nl>
2011-06-15 22:34:21 +0200

    plugin/df: collectd 5 compatibility

--- plugin/df.php
+++ plugin/df.php
@@ -8,25 +8,35 @@ require_once 'inc/collectd.inc.php';
 
 # LAYOUT
 #
-# df/
-# df/df-XXXX.rrd
+# df-X/df_complex-free.rrd
+# df-X/df_complex-reserved.rrd
+# df-X/df_complex-used.rrd
 
 $obj = new Type_GenericStacked($CONFIG);
-$obj->data_sources = array('free', 'used');
+$obj->data_sources = array('value');
+$obj->order = array('reserved', 'free', 'used');
 $obj->ds_names = array(
+	'reserved' => 'Reserved',
 	'free' => 'Free',
 	'used' => 'Used',
 );
 $obj->colors = array(
+	'reserved' => 'aaaaaa',
 	'free' => '00ff00',
 	'used' => 'ff0000',
 );
 $obj->width = $width;
 $obj->heigth = $heigth;
 
-$obj->rrd_title = sprintf('Free space (%s)', $obj->args['tinstance']);
+$obj->rrd_title = sprintf('Free space (%s)', $obj->args['pinstance']);
 $obj->rrd_vertical = 'Bytes';
 $obj->rrd_format = '%5.1lf%sB';
+
+# backwards compatibility
+if ($CONFIG['version'] < 5) {
+	$obj->data_sources = array('free', 'used');
+	$obj->rrd_title = sprintf('Free space (%s)', $obj->args['tinstance']);
+}
 
 collectd_flush($obj->identifiers);
 $obj->rrd_graph();
