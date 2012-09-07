238e13fdaef8eb931e6665edcd24c4039f4dd1ff
Pim van den Berg <pim@nethuis.nl>
2012-09-07 15:19:50 +0200

    plugin: add powerdns plugin
    This is a very basic PowerDNS plugin.

---
+++ plugin/powerdns.php
@@ -0,0 +1,18 @@
+<?php
+
+# Collectd PowerDNS plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/Default.class.php';
+require_once 'inc/collectd.inc.php';
+
+
+$obj = new Type_Default($CONFIG);
+$obj->width = $width;
+$obj->heigth = $heigth;
+
+$obj->rrd_title = sprintf('PowerDNS %s (%s)', $obj->args['type'], $obj->args['pinstance']);
+$obj->rrd_format = '%5.1lf%s';
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
