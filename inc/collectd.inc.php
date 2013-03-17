60fb79ac97869b8333621380828dd1b84a069990
Rohit Bhute <rvbhute@gmail.com>
2013-03-17 13:40:38 +0530

    add feature to show a subset of graphs from a plugin on overview page

--- inc/collectd.inc.php
+++ inc/collectd.inc.php
@@ -147,14 +147,25 @@ function plugin_sort($data) {
 }
 
 # generate graph url's for a plugin of a host
-function graphs_from_plugin($host, $plugin) {
+function graphs_from_plugin($host, $plugin, $overview=false) {
 	global $CONFIG;
 
 	$plugindata = collectd_plugindata($host, $plugin);
 	$plugindata = group_plugindata($plugindata);
 	$plugindata = plugin_sort($plugindata);
 
+	$f = array();
+
+	if ($overview == true && isset($CONFIG['overview_filter'][$plugin])) {
+		$f = $CONFIG['overview_filter'][$plugin];
+	}
+
 	foreach ($plugindata as $items) {
+
+		if (!empty($f) && ($f !== array_intersect_assoc($f, $items))) {
+			continue;
+		}
+
 		$items['h'] = $host;
 
 		$time = array_key_exists($plugin, $CONFIG['time_range'])
