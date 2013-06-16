21cf80f378898265e239c3c444699669d71a0b36
Conrad Kostecki <ConiKost@gmx.de>
2013-06-16 16:09:03 +0200

    plugin: add openvpn plugin
    Source: http://pastebin.com/5vNDuKaZ

---
+++ plugin/openvpn.php
@@ -0,0 +1,42 @@
+<?php
+
+# Collectd OpenVPN plugin
+
+require_once 'conf/common.inc.php';
+require_once 'inc/collectd.inc.php';
+
+## LAYOUT
+# openvpn-XXXX/
+# openvpn-XXXX/if_octets-XXXX.rrd
+# openvpn-XXXX/users-XXXX.rrd
+
+switch(GET('t')) {
+	case 'if_octets':
+		require_once 'type/GenericIO.class.php';
+		$obj = new Type_GenericIO($CONFIG);
+		$obj->ds_names = array(
+			'rx' => 'Receive',
+			'tx' => 'Transmit',
+		);
+		$obj->colors = array(
+			'rx' => '0000ff',
+			'tx' => '00b000',
+		);
+		$obj->rrd_title = sprintf('Traffic (%s)', $obj->args['pinstance']);
+		$obj->rrd_vertical = '%s per second';
+		$obj->rrd_format = '%5.1lf%s';
+	break;
+	case 'users':
+		require_once 'type/Default.class.php';
+		$obj = new Type_Default($CONFIG);
+		$obj->ds_names = array(
+			'value' => 'Users',
+		);
+		$obj->rrd_title = sprintf('Users (%s)', $obj->args['pinstance']);
+		$obj->rrd_vertical = 'Users';
+		$obj->rrd_format = '%.1lf';
+	break;
+}
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
