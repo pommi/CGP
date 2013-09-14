8f38215931c9840f12da0657d8ff922e0b1bc11d
Pim van den Berg <pim@nethuis.nl>
2013-09-14 11:40:41 +0200

    solve undefined index cat in index.php
    Reported-by: tbleiker (https://github.com/pommi/CGP/issues/25)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ html_start();
 $h = array();
 
 # show all categorized hosts
-if (is_array($CONFIG['cat'])) {
+if (isset($CONFIG['cat']) && is_array($CONFIG['cat'])) {
 	foreach($CONFIG['cat'] as $cat => $hosts) {
 		host_summary($cat, $hosts);
 		$h = array_merge($h, $hosts);
