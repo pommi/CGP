fc533de432480560def92143d03ada40957de2ef
Pim van den Berg <pim@nethuis.nl>
2013-10-07 21:22:09 +0200

    type/GenericStacked: make stack NaN-safe using ADDNAN instead of +
    Reported-By: Julien Genestoux <julien.genestoux@gmail.com>
                 (https://github.com/pommi/CGP/issues/24)

--- type/GenericStacked.class.php
+++ type/GenericStacked.class.php
@@ -37,7 +37,7 @@ class Type_GenericStacked extends Type_Default {
 			if ($i == (count($sources)-1))
 				$rrdgraph[] = sprintf('CDEF:area_%s=avg_%1$s', crc32hex($sources[$i]));
 			else
-				$rrdgraph[] = sprintf('CDEF:area_%s=area_%s,avg_%1$s,+', crc32hex($sources[$i]), crc32hex($sources[$i+1]));
+				$rrdgraph[] = sprintf('CDEF:area_%s=area_%s,avg_%1$s,ADDNAN', crc32hex($sources[$i]), crc32hex($sources[$i+1]));
 		}
 
 		$c = 0;
