325042f112f17deac5e03e4708b19536c6a21bab
Ben Cox <Ben@Benjojo.co.uk>
2013-11-30 13:37:09 +0000

    Fix a DOS vun in graph.php
    The issue happened when a user attemped to request a huge image, thus consuming all memory on the host.

--- graph.php
+++ graph.php
@@ -7,18 +7,21 @@ $plugin = validate_get(GET('p'), 'plugin');
 $width = empty($_GET['x']) ? $CONFIG['width'] : $_GET['x'];
 $heigth = empty($_GET['y']) ? $CONFIG['heigth'] : $_GET['y'];
 
-if (validate_get(GET('h'), 'host') === NULL) {
-	error_log('CGP Error: plugin contains unknown characters');
-	error_image();
+if(($width * $heigth) < 280001) {
+	if (validate_get(GET('h'), 'host') === NULL) {
+		error_log('CGP Error: plugin contains unknown characters');
+		error_image();
+	}
+
+	if (!file_exists($CONFIG['webdir'].'/plugin/'.$plugin.'.php')) {
+		error_log(sprintf('CGP Error: plugin "%s" is not available', $plugin));
+		error_image();
+	}
+
+	# load plugin
+	include $CONFIG['webdir'].'/plugin/'.$plugin.'.php';
+} else {
+	die("The image you resquested is too large. Please reduce the size of the image you are attempting to fetch.");
 }
-
-if (!file_exists($CONFIG['webdir'].'/plugin/'.$plugin.'.php')) {
-	error_log(sprintf('CGP Error: plugin "%s" is not available', $plugin));
-	error_image();
-}
-
-# load plugin
-include $CONFIG['webdir'].'/plugin/'.$plugin.'.php';
-
 
 ?>
