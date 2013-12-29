8a83bc09aac260c9b35f5ab68d6685fad44c3113
Pim van den Berg <pim@nethuis.nl>
2013-12-29 17:00:23 +0100

    plugin/tail: collectd < 5 backwards compatibility
    Closes: https://github.com/pommi/CGP/issues/36

--- plugin/tail.php
+++ plugin/tail.php
@@ -12,5 +12,20 @@ $obj = new Type_Default($CONFIG);
 $obj->rrd_title = sprintf('tail: %s (%s)', $obj->args['pinstance'], $obj->args['type']);
 $obj->rrd_format = '%5.1lf%s';
 
+# backwards compatibility
+if ($CONFIG['version'] < 5) {
+	if (strcmp($obj->args['type'], 'gauge') != 0) {
+		$obj->data_sources = array('count');
+		if (count($obj->ds_names) == 1) {
+			$obj->ds_names['count'] = $obj->ds_names['value'];
+			unset($obj->ds_names['value']);
+		}
+		if (count($obj->colors) == 1) {
+			$obj->colors['count'] = $obj->colors['value'];
+			unset($obj->colors['value']);
+		}
+	}
+}
+
 collectd_flush($obj->identifiers);
 $obj->rrd_graph();
