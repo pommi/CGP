93164fd8d4027666d05ff33d0f05e620fd3dca33
Brian ONeill <btoneill@walleyesoftware.com>
2015-08-07 10:46:44 -0400

    Added a bunch of changes...

---
+++ plugin/zfs_arc.php
@@ -0,0 +1,99 @@
+<?php
+# Collectd zfs_arc plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/Default.class.php';
+require_once 'inc/collectd.inc.php';
+
+## LAYOUT
+# zfs_arc/
+# zfs_arc/arc_counts-hits.rrd
+# zfs_arc/arc_counts-misses.rrd
+# zfs_arc/arc_l2_bytes.rrd
+# zfs_arc/arc_l2_size.rrd
+# zfs_arc/arc_ratio-L1.rrd
+# zfs_arc/arc_ratio-L2.rrd
+# zfs_arc/arc_size.rrd
+
+$obj = new Type_Default($CONFIG);
+$obj->rrd_format = '%5.1lf%s';
+
+switch($obj->args['type']) {
+	case 'arc_counts':
+		$obj->data_sources = array(
+			'demand_data',
+			'demand_metadata',
+			'prefetch_data',
+			'prefetch_metadata',
+		);
+		$obj->colors = array(
+			'hits-demand_data' => 'ff0000',
+			'misses-demand_data' => '880000',
+			'hits-demand_metadata' => '00ff00',
+			'misses-demand_metadata' => '00aa00',
+			'hits-prefetch_data' => '0000ff',
+			'misses-prefetch_data' => '00f0f0',
+			'hits-prefetch_metadata' => 'ff00ff',
+			'misses-prefetch_metadata' => '888800',
+		);
+		$obj->ds_names = array(
+			'hits-demand_data' => 'data hits',
+			'misses-demand_data' => 'metadata misses',
+			'hits-demand_metadata' => 'metadata hits',
+			'misses-demand_metadata' => 'metadata misses',
+			'hits-prefetch_data' => 'prefetch data hits',
+			'misses-prefetch_data' => 'prefetch data misses',
+			'hits-prefetch_metadata' => 'prefetch metadata hits',
+			'misses-prefetch_metadata' => 'prefetch metadata misses',
+		);
+		$obj->rrd_title = 'arc counts';
+		$obj->rrd_vertical = 'count';
+		break;
+	case 'cache_size':
+#		$obj->data_sources = array('current','target','minlimit','maxlimit');
+#		$obj->order = array(
+#			'current',
+#			'target',
+#			'minlimit',
+#			'maxlimit',
+#		);
+		$obj->rrd_title = 'Arc size';
+		$obj->rrd_vertical = 'bytes';
+		break;
+	case 'arc_l2_bytes':
+		$obj->data_sources = array(
+			'write',
+			'read',
+		);
+		$obj->ds_names = array(
+			'write' => 'Write',
+			'read'  => 'Read',
+		);
+		$obj->colors = array(
+			'write' => 'ff0000',
+			'read'  => '0000ff',
+		);
+		$obj->rrd_title = 'Arc L2 bytes';
+		$obj->rrd_vertical = 'bytes';
+		break;
+	case 'arc_l2_size':
+		$obj->data_sources = array(
+			'value',
+		);
+		$obj->ds_names = array(
+			'value'   => 'Bytes',
+		);
+		$obj->colors = array(
+			'value' => '0000ff',
+		);
+		$obj->rrd_title = 'Arc L2 size';
+		$obj->rrd_vertical = 'bytes';
+		break;
+	case 'arc_ratio':
+		$obj->data_sources = array('value');
+		$obj->rrd_title = 'Arc ratio';
+		$obj->rrd_vertical = 'ratio';
+		break;
+}
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
---
+++ type/GenericFilled.class.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once 'Base.class.php';
+
+class Type_GenericFilled extends Type_Base {
+	
+	function rrd_gen_graph() {
+		$rrdgraph = $this->rrd_options();
+
+		$sources = $this->rrd_get_sources();
+
+		$raw = null;
+		if ($this->scale)
+			$raw = '_raw';
+		$i=0;
+		foreach ($this->tinstances as $tinstance) {
+			foreach ($this->data_sources as $ds) {
+				$rrdgraph[] = sprintf('DEF:min_%s%s=%s:%s:MIN', crc32hex($sources[$i]), $raw, $this->parse_filename($this->files[$tinstance]), $ds);
+				$rrdgraph[] = sprintf('DEF:avg_%s_raw=%s:%s:AVERAGE', crc32hex($sources[$i]), $this->parse_filename($this->files[$tinstance]), $ds);
+				$rrdgraph[] = sprintf('DEF:max_%s%s=%s:%s:MAX', crc32hex($sources[$i]), $raw, $this->parse_filename($this->files[$tinstance]), $ds);
+				$i++;
+			}
+		}
+		if ($this->scale) {
+			$i=0;
+			foreach ($this->tinstances as $tinstance) {
+				foreach ($this->data_sources as $ds) {
+					$rrdgraph[] = sprintf('CDEF:min_%s=min_%1$s_raw,%s,*', crc32hex($sources[$i]), $this->scale);
+					$rrdgraph[] = sprintf('CDEF:avg_%s=avg_%1$s_raw,%s,*', crc32hex($sources[$i]), $this->scale);
+					$rrdgraph[] = sprintf('CDEF:max_%s=max_%1$s_raw,%s,*', crc32hex($sources[$i]), $this->scale);
+					if ($i == 1)
+						$rrdgraph[] = sprintf('CDEF:avg_%s_neg=avg_%1$s_raw,%s%s,*', crc32hex($sources[$i]), $this->negative_io ? '-' : '', $this->scale);
+					if ($this->percentile)
+						$rrdgraph[] = sprintf('VDEF:pct_%1$s=avg_%1$s,%2$s,PERCENT', crc32hex($sources[$i]), $this->percentile);
+					$i++;
+				}
+			}
+		}
+
+		$rrdgraph[] = sprintf('CDEF:overlap=avg_%s,avg_%s_neg,LT,avg_%1$s,avg_%2$s_neg,IF',
+						crc32hex($sources[0]), crc32hex($sources[1]));
+
+		$i = 0;
+		foreach($sources as $source) {
+			$rrdgraph[] = sprintf('AREA:avg_%s%s#%s', crc32hex($source), $i == 1 ? '_neg' : '', $this->get_faded_color($this->colors[$source]));
+			$i++;
+		}
+
+		$rrdgraph[] = sprintf('AREA:overlap#%s',
+			$this->get_faded_color(
+				$this->get_faded_color($this->colors[$sources[0]]),
+				$this->get_faded_color($this->colors[$sources[1]])
+			)
+		);
+
+		$i = 0;
+		foreach($sources as $source) {
+			$legend = empty($this->legend[$source]) ? $source : $this->legend[$source];
+			$rrdgraph[] = sprintf('LINE1:avg_%s%s#%s:%s', crc32hex($source), $i == 1 ? '_neg' : '', $this->colors[$source], $this->rrd_escape($legend));
+			$rrdgraph[] = sprintf('GPRINT:min_%s:MIN:%s Min,', crc32hex($source), $this->rrd_format);
+			$rrdgraph[] = sprintf('GPRINT:avg_%s:AVERAGE:%s Avg,', crc32hex($source), $this->rrd_format);
+			$rrdgraph[] = sprintf('GPRINT:max_%s:MAX:%s Max,', crc32hex($source), $this->rrd_format);
+			$rrdgraph[] = sprintf('GPRINT:avg_%s:LAST:%s Last\l', crc32hex($source), $this->rrd_format);
+			$i++;
+		}
+
+		if ($this->percentile) {
+			$rrdgraph[] = 'COMMENT: \l';
+			foreach($sources as $source) {
+				$legend = empty($this->legend[$source]) ? $source : $this->legend[$source];
+				$rrdgraph[] = sprintf('HRULE:pct_%s#%s:%sth Percentile %s', crc32hex($source), $this->get_faded_color($this->colors[$source], '000000', 0.6), $this->percentile, $this->rrd_escape($legend));
+				$rrdgraph[] = sprintf('GPRINT:pct_%s:%s\l', crc32hex($source), $this->rrd_format);
+			}
+		}
+
+		return $rrdgraph;
+	}
+}
